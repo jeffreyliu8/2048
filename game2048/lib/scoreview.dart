ddabc879587234c67d2660758198ebd25ec9509f
Jeffrey Liu <jeffreyliu8@gmail.com>
2019-03-09 14:06:35 -0800

    add score view

---
+++ game2048/lib/scoreview.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ScoreView extends StatefulWidget {
+  const ScoreView({
+    Key key,
+    this.title: "title",
+
+  }) : super(key: key);
+
+  final String title;
+
+
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ScoreViewState();
+  }
+}
+
+class _ScoreViewState extends State<ScoreView> {
+  int _score = 0;
+
+  void increaseScore() {
+    setState(() {
+      _score += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Container(
+          color: Colors.red,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                Text(widget.title),
+                Text(_score.toString()),
+              ],
+            ),
+          )),
+    );
+  }
+}
